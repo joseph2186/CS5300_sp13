ebe4a2b9826584ac70336168b8806da84d42faaa
Geet Varma <geet@GV-VB.(none)>
2013-04-30 01:15:50 -0400

    reducer Code completed

---
+++ assignments/project2/PageRankBlocked/src/MetadataValue.java
@@ -0,0 +1,59 @@
+public class MetadataValue {
+	public static final int CONST_INT_NODES_SEPARATOR = 4;
+	private String nodeID;
+	private String pageRank;
+	private int outdegree;
+	private String listOfNodes;
+
+	public MetadataValue(String values)
+	{
+		String[] valuesMinusNode = values.split(PageRankReducerBlocked.DELIMITER, CONST_INT_NODES_SEPARATOR);
+		this.nodeID = valuesMinusNode[0];
+		this.pageRank = valuesMinusNode[1];
+		this.outdegree = Integer.parseInt(valuesMinusNode[2]);
+		this.listOfNodes = valuesMinusNode[3];
+	}
+	
+	public String getNodeID() {
+		return nodeID;
+	}
+
+	public String getPageRank() {
+		return pageRank;
+	}
+
+	public int getOutdegree() {
+		return outdegree;
+	}
+
+	public String getListOfNodes() {
+		return listOfNodes;
+	}
+
+	public void setPageRank(String pageRank) {
+		this.pageRank = pageRank;
+	}
+
+	public void setPageRankFromDouble(double prDouble)
+	{
+		this.pageRank = String.valueOf(prDouble);
+	}
+	
+	public double getPageRankAsDouble()
+	{
+		return Double.parseDouble(this.pageRank);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getPageRank()).append(PageRankReducerBlocked.DELIMITER).append(getOutdegree())
+				.append(PageRankReducerBlocked.DELIMITER).append(getListOfNodes());
+
+		return sb.toString();
+	}
+
+	public double getResidue(double newPR) {
+		return Math.abs((this.getPageRankAsDouble() - newPR)/newPR);
+	}
+
+}
--- assignments/project2/PageRankBlocked/src/PageRankReducerBlocked.java
+++ assignments/project2/PageRankBlocked/src/PageRankReducerBlocked.java
@@ -1,67 +1,111 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 
+public class PageRankReducerBlocked extends Reducer<Text, Text, Text, Text> {
 
-public class PageRankReducerBlocked extends Reducer<Text, Text, Text, Text>{
+	public static final int CONST_INT_NO_OF_ITERATIONS = 6;
+	public static final double CONST_TOTAL_NODES = 685229.0;
+	public static final double CONST_DOUBLE_DAMPING_FACTOR = 0.85;
+	public static final String DELIMITER = "\t";
+	public static final Double CONST_DOUBLE_THRESHOLD = 0.001;
+
+	HashMap<String, ArrayList<String>> BEMap = new HashMap<String, ArrayList<String>>();
+	HashMap<String, ArrayList<BCData>> bcDataMap = new HashMap<String, ArrayList<BCData>>();
+	HashMap<String, Double> nprMap = new HashMap<String, Double>();
+	HashMap<String, MetadataValue> mdMap = new HashMap<String, MetadataValue>();
+	HashSet<String> setOfNodes = new HashSet<String>();
+
 	@Override
-    protected void reduce(Text key, Iterable<Text> values, 
-            Context context)
-            throws IOException, InterruptedException {
-        Iterator<Text> itr = values.iterator();
-        Text input = new Text();
-        String[] inputTokens = null;
-        Double pageRank = (Double) 0.0;
-        Double oldPageRank = (Double) 0.0;
-        Double residual = (Double) 0.0;
-        Double threshold = (Double) 0.01;
-        ArrayList<String> edgeList = new ArrayList<String>();
-        String output = "";
-        
-        while (itr.hasNext()) {
-            input = itr.next();
-            String inputStr = input.toString();
-            inputTokens = inputStr.split(" ");
-            if (inputTokens.length == 1){
-            	pageRank = pageRank + Double.parseDouble(inputTokens[0]);
-            } else {
-            	oldPageRank = Double.parseDouble(inputTokens[0]);
-            	for (int i=0 ; i < inputTokens.length ; i++){
-            		edgeList.add(i, new String(inputTokens[i]));
-            	}
-            }
-            
-        }
-        if (Double.compare(pageRank, threshold) > 1){
-        	residual = (oldPageRank - pageRank)/pageRank;
-        } else {
-        	//System.out.println("error!!!");
-        }
-        if (Double.compare(threshold, residual) > 1){
-        	//System.out.println("converged!!");
-        	pageRank = oldPageRank;
-        }
-        
-        //TODO: check if this condition is correct
-        if (edgeList.size() > 0) {
-        	edgeList.remove(0);
-        }
-        edgeList.add(0, new String(String.valueOf(pageRank)));
-        for (int i = 0 ; i < edgeList.size() - 1 ; i++){
-        	output += edgeList.get(i)+"\t";
-        }
-        output += edgeList.get(edgeList.size() - 1);
-        
-        Text outputText = new Text(output);
-        context.getCounter(PAGE_RANK_COUNTER.RESIDUAL).increment(1);
-        //System.out.println("counter-->"+context.getCounter(TEST_COUNTER.COUNTER_1).getValue());
-        
-        context.write(key, outputText);
-        cleanup(context);
-    }
+	protected void reduce(Text key, Iterable<Text> values, Context context)
+			throws IOException, InterruptedException {
 
+		MetadataValue mDV = null;
+		BCData bcData = null;
+		Iterator<Text> iterator = values.iterator();
+
+		while (iterator.hasNext()) {
+			Text line = (Text) iterator.next();
+			String[] lineTokens = line.toString().split(DELIMITER);
+
+			if (lineTokens.length == 2) {
+				// BE inputs
+
+				if (!BEMap.containsKey(lineTokens[1])) {
+					ArrayList<String> list = new ArrayList<String>();
+					BEMap.put(lineTokens[1], list);
+				}
+				BEMap.get(lineTokens[1]).add(lineTokens[0]);
+			} else if (lineTokens.length == 3) {
+				bcData = new BCData(line.toString());
+				if (!bcDataMap.containsKey(lineTokens[1])) {
+					ArrayList<BCData> list = new ArrayList<BCData>();
+					bcDataMap.put(lineTokens[1], list);
+				}
+				bcDataMap.get(lineTokens[1]).add(bcData);
+			} else {
+				setOfNodes.add(lineTokens[0]);
+				mDV = new MetadataValue(line.toString());
+				mdMap.put(lineTokens[0], mDV);
+			}
+		}
+
+		// Calculate new PR values
+		// Logic!!?
+		double residual = 0;
+		int counter = 0;
+		do {
+			residual = iterateBlockOnce();
+			++counter;
+		} while (Double.compare(residual, CONST_DOUBLE_THRESHOLD) > 0
+				&& counter < CONST_INT_NO_OF_ITERATIONS);
+
+		String output = "";
+		Text outputTextVal = null;
+		Text outputTextKey = null;
+
+		for (MetadataValue mdv : mdMap.values()) {
+			output = mdv.toString();
+			outputTextVal = new Text(output);
+			outputTextKey = new Text(mdv.getNodeID());
+			context.write(outputTextKey, outputTextVal);
+		}
+
+		cleanup(context);
+	}
+
+	private double iterateBlockOnce() {
+		double residual = 0d;
+		nprMap = new HashMap<String, Double>();
+		for (String v : setOfNodes) {
+			nprMap.put(v, 0d);
+			for (String u : BEMap.get(v)) {
+				double newPR = mdMap.get(u).getPageRankAsDouble()
+						/ (double) mdMap.get(u).getOutdegree();
+				newPR += nprMap.get(v);
+				nprMap.put(v, newPR);
+			}
+
+			for (BCData bcd : bcDataMap.get(v)) {
+				double newPR = bcd.getR();
+				newPR += nprMap.get(v);
+				nprMap.put(v, newPR);
+			}
+			double newPR = CONST_DOUBLE_DAMPING_FACTOR * nprMap.get(v)
+					+ (1d - CONST_DOUBLE_DAMPING_FACTOR) / CONST_TOTAL_NODES;
+			nprMap.put(v, newPR);
+
+			residual += mdMap.get(v).getResidue(newPR);
+
+			// we do it here since we need the old value in the above
+			// computation
+			mdMap.get(v).setPageRankFromDouble(newPR);
+		}
+		return residual;
+	}
 }
